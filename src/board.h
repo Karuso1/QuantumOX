155347e7e76dfeed2b1ee3614b8e44a4ec0d9af4
Kartik Pawar <karuman2013@gmail.com>
2026-01-16 19:00:31 +0530

    fix: make set_side_to_move std::string

--- src/board.h
+++ src/board.h
@@ -55,7 +55,7 @@ namespace QuantumOX {
         void fill_from_list(const std::vector<int>& moves);
         bool validate_tttn(const std::string& tttn_str) const;
         void load_tttn(const std::string& tttn_str);
-        void set_side_to_move(char player);
+        void set_side_to_move(const std::string& player);
         void reset();
         std::string to_string() const;
         std::string get_side_to_move() const;
